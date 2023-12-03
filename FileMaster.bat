d15d84beee34fc0413bfea4838e168de75d19f5f
XinTong Li <kicw118@gmail.com>
2023-12-03 17:27:09 -0500

    Update FileMaster.bat

--- FileMaster.bat
+++ FileMaster.bat
@@ -7,17 +7,17 @@ set "batchFilePath=%~f0"
 
 REM -- Initial Choice
 echo Choose a function:
-echo 1. Duplicate Remove
-echo 2. ExtOrganizer
+echo 1. ExtOrganizer
+echo 2. Duplicate Remove
 set /p functionChoice="Enter your choice (1 or 2): "
 
 REM -- Confirm choice
 if "%functionChoice%"=="1" (
-    echo You have chosen Duplicate Remove.
-    goto duplicateRemove
-) else if "%functionChoice%"=="2" (
     echo You have chosen ExtOrganizer.
     goto extOrganizer
+) else if "%functionChoice%"=="2" (
+    echo You have chosen Duplicate Remove.
+    goto duplicateRemove
 ) else (
     echo Invalid choice. Please try again.
     goto :EOF
@@ -27,6 +27,26 @@ if "%functionChoice%"=="1" (
 REM -- Duplicate Remove functionality
 echo Starting Duplicate Remove...
 echo.
+
+REM -- Choose mode for Duplicate Remove
+:chooseModeDuplicateRemove
+echo Choose a mode for Duplicate Remove:
+echo 1. Fast Mode (Files will be permanently deleted)
+echo 2. Normal Mode (Files will be moved to Recycle Bin)
+set /p modeChoice="Enter your choice (1 or 2): "
+
+REM -- Confirm choice
+if "%modeChoice%"=="1" (
+    echo You have chosen Fast Mode. Files will be permanently deleted.
+) else if "%modeChoice%"=="2" (
+    echo You have chosen Normal Mode. Files will be moved to Recycle Bin.
+) else (
+    echo Invalid choice. Please try again.
+    goto chooseModeDuplicateRemove
+)
+
+set /p confirm="Are you sure? (Y/N): "
+if /i not "%confirm%"=="Y" goto chooseModeDuplicateRemove
 
 REM -- Ask the user for file extension for duplicate removal
 set /p fileExt="Enter file extension for Duplicate Remove (e.g., txt) or type 'all' for all files: "
@@ -41,21 +61,49 @@ if /i "%fileExt%"=="all" (
 REM -- Change directory to the script's location
 cd /d "%~dp0"
 
+REM -- Initialize progress bar
+set "progress=0"
+set "totalFiles=0"
+
+REM -- Count total files for progress calculation
+for %%F in (%pattern%) do set /a totalFiles+=1
+
 REM -- Iterate over files based on the pattern for duplicate removal
 for %%F in (%pattern%) do (
+    set /a progress+=1
+    set /a percent=progress*100/totalFiles
+    cls
+    echo Progress: !percent!%% - Script by Kinnto Lee
+    echo.
+
+    set "maxName=%%F"
+    REM -- Compare with other files
     for %%G in (%pattern%) do (
+        REM -- Check if file size is the same and not the same file
         if %%~zF==%%~zG if not "%%F"=="%%G" (
-            if "%%~nG" gtr "%%~nF" (
-                del "%%F"
+            REM -- Check if the name length is longer
+            if "!maxName!"=="%%F" (
+                if "%%~nG" gtr "%%~nF" (
+                    set "maxName=%%G"
+                    if "%modeChoice%"=="1" (
+                        del "%%F"
+                    ) else (
+                        powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile('%%F', 'OnlyErrorDialogs', 'SendToRecycleBin')}"
+                    )
+                )
             ) else (
-                del "%%G"
+                if "%modeChoice%"=="1" (
+                    del "%%G"
+                ) else (
+                    powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile('%%G', 'OnlyErrorDialogs', 'SendToRecycleBin')}"
+                )
             )
         )
     )
 )
 
 echo Duplicate Remove Complete.
-goto endScript
+goto deleteEmptyFolders
 
 :extOrganizer
 REM -- ExtOrganizer functionality
@@ -77,9 +125,12 @@ for /r %%F in (*) do (
     )
 )
 
-REM -- Delete original folders if empty
-for /d %%D in (*) do (
-    dir "%%D" | find "0 File(s)" > nul && rmdir "%%D"
+:deleteEmptyFolders
+REM -- Delete empty folders and folders with a size of 0 bytes
+for /f %%D in ('dir /ad /s /b ^| sort /R') do (
+    dir "%%D" | findstr /C:"File(s)" /C:"Dir(s)" | findstr /C:"0 File(s)" /C:"0 Dir(s)" > nul && (
+        rmdir "%%D"
+    )
 )
 
 echo ExtOrganizer Complete.
